1d3f5ef566c368b94720b6c563804db143d53762
gabriel80546 <gabriel80546@gmail.com>
2018-08-25 21:42:26 -0300

    asdfasdfijwer

--- view.sh
+++ view.sh
@@ -1,1 +1,2 @@
-vim -p BootLoader.gex ProtectedMode.gex Redirect.gex KernelFunctions.gex Kernel.gex utils.gex
+vim -p KernelFunctions.gex Kernel.gex utils.gex
+#vim -p BootLoader.gex ProtectedMode.gex Redirect.gex KernelFunctions.gex Kernel.gex utils.gex
