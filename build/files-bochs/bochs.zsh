068a0d7200a39637324a17cc358f133d3d0b2ee1
gabriel80546 <gabriel80546@gmail.com>
2018-09-05 09:05:46 -0300

    asdfasdfijwer

--- build/files-bochs/bochs.zsh
+++ build/files-bochs/bochs.zsh
@@ -11,6 +11,7 @@ rm iso.bin
 cp kernel.img iso.bin
 cd iso/
 ./make.zsh
-echo -e "\033[1;36mbochs\033[0m"
+rm ../iso.bin
+#echo -e "\033[1;36mbochs\033[0m"
 #bochs 'ata1-master:  type=cdrom, path=/home/gabriel/lks/osdev/iso/myos.iso, status=inserted' 'boot: cdrom' -q -rc /home/gabriel/lks/osdev/build/bochs.sh.txt
 #bochs -f bochsrc.txt
